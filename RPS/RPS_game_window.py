f8852042e5e134fca95dede4576021a45e2f7f4f
BurningPheonix <59739923+BurningPheonix@users.noreply.github.com>
2020-07-21 22:39:18 +0530

    Update RPS_game_window.py

--- RPS/RPS_game_window.py
+++ RPS/RPS_game_window.py
@@ -1,3 +1,4 @@
+import os
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -7,12 +8,29 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
+os.chdir(os.cwd())
+import main_body
+n=int()
 class MyGrid(Widget):
-	pass
-
+	p_wins = ObjectProperty(None)
+	com_wins = ObjectProperty(None)
+	def win_count(self):
+		self.p_wins.text = self.p_wins.text
+		self.com_wins.text = self.com_wins.text
+		
+	def rock(self):
+		n=-1
+	def paper(self):
+		n=0
+	def scissor(self):
+		n=1
+	
+	def main(self):
+		p_wins,com_wins=main_body.main(n)
+		win_count()
 class RPSApp(App):
-    def build(self):
-        return MyGrid()
+	def build(self):
+		return MyGrid()
 
 if __name__=="__main__":
-    RPSApp().run()
+	RPSApp().run()
