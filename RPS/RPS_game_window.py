c597f837619ea5e9ad253e3ae1f12d3e016624a3
BurningPheonix <59739923+BurningPheonix@users.noreply.github.com>
2020-07-22 09:18:55 +0530

    Update RPS_game_window.py

--- RPS/RPS_game_window.py
+++ RPS/RPS_game_window.py
@@ -15,22 +15,28 @@ n = 0
 class MyGrid(Widget):
 		global p_wins
 		global com_wins
+		global comp_choice
+		global p_choice
 		p_wins = ObjectProperty(None)
 		com_wins = ObjectProperty(None)
-		
+		comp_choice = ObjectProperty(None)
+		p_choice = ObjectProperty(None)
 		def rock(self):
-				n=-1
+			global n
+			n=-1
 		def paper(self):
-				n=0
+			global n
+			n=0
 		def scissor(self):
-				n=1
-		
+			global n
+			n=1
 		def main(self):
-				p_wins,com_wins=main_body.main(n)
-				
-				self.p_wins.text = str(p_wins)
-				self.com_wins.text = str(com_wins)
-
+			p_wins, com_wins, comp_choice ,p_choice = main_body.main(n)
+			self.p_wins.text = str(p_wins)
+			self.com_wins.text = str(com_wins)
+			self.comp_choice.text = str(comp_choice)
+			self.p_choice.text = str(p_choice)
+			
 class RPSApp(App):
 		def build(self):
 				return MyGrid()
