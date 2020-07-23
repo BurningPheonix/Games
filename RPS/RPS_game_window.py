a7a292f6a0efe33e02eddf44d27a803774f69636
BurningPheonix <59739923+BurningPheonix@users.noreply.github.com>
2020-07-23 09:43:45 +0530

    Update RPS_game_window.py

--- RPS/RPS_game_window.py
+++ RPS/RPS_game_window.py
@@ -13,33 +13,47 @@ import main_body
 global n
 n = 0
 class MyGrid(Widget):
-		global p_wins
-		global com_wins
-		global comp_choice
-		global p_choice
-		p_wins = ObjectProperty(None)
-		com_wins = ObjectProperty(None)
-		comp_choice = ObjectProperty(None)
-		p_choice = ObjectProperty(None)
-		def rock(self):
-			global n
-			n=-1
-		def paper(self):
-			global n
-			n=0
-		def scissor(self):
-			global n
-			n=1
-		def main(self):
-			p_wins, com_wins, comp_choice ,p_choice = main_body.main(n)
-			self.p_wins.text = str(p_wins)
-			self.com_wins.text = str(com_wins)
-			self.comp_choice.text = str(comp_choice)
-			self.p_choice.text = str(p_choice)
-			
+                global p_wins
+                global com_wins
+                global comp_choice
+                global p_choice
+                p_wins = ObjectProperty(None)
+                com_wins = ObjectProperty(None)
+                comp_choice = ObjectProperty(None)
+                p_choice = ObjectProperty(None)
+                def rock(self):
+                        global n
+                        n=-1
+                def paper(self):
+                        global n
+                        n=0
+                def scissor(self):
+                        global n
+                        n=1
+                def main(self):
+                        global p_wins
+                        global com_wins
+                        global comp_choice
+                        global p_choice
+                        p_wins, com_wins, comp_choice ,p_choice = main_body.main(n)
+                        self.p_wins.text = str(p_wins)
+                        self.com_wins.text = str(com_wins)
+                        self.comp_choice.text = str(comp_choice)
+                        self.p_choice.text = str(p_choice)
+                def reset(self):
+                        global p_wins
+                        global com_wins
+                        p_wins = 0
+                        com_wins = 0
+                        self.p_wins.text = "0"
+                        self.com_wins.text = "0"
+                        self.comp_choice.text = "The CPU's choice is shown here"
+                        self.p_choice.text = "Your choice is shown here"
+                        main_body.rezero()
+                        
 class RPSApp(App):
-		def build(self):
-				return MyGrid()
+                def build(self):
+                                return MyGrid()
 
 if __name__=="__main__":
-		RPSApp().run()
+                RPSApp().run()
